20753e3a843f9102160db0aab478aef482e5b68c
kaiakamatsu <108591793+kaiakamatsu@users.noreply.github.com>
2024-11-21 17:53:40 -0800

    plot sum PRS

--- scripts_kai/PRS/collect_plot/plot_PRS.R
+++ scripts_kai/PRS/collect_plot/plot_PRS.R
@@ -9,18 +9,20 @@ setting <- "C:/Users/kaiak/OneDrive/Documents/GESTALT/N50kM10k_10traits_rg0.05_r
 p_thresholds <- c('0.000005', '0.001', '0.05')
 num_pheno <- 10
 #num_pheno <- 5
-#heritabilities <- c(0.4, 0.3, 0.15, 0.5, 0.2, 'MaxH', 'SUM_PRS')
-heritabilities <- c(rep(0.1, times = num_pheno), 'MaxH', 'SUM_PRS')
+#heritabilities <- c(0.4, 0.3, 0.15, 0.5, 0.2, 'MaxH', 'SUM_MaxH', 'SUM')
+heritabilities <- c(rep(0.1, times = num_pheno), 'MaxH', 'SUM_MaxH', 'SUM')
 
 # --- plot all p value settings separately
 for (p_current in p_thresholds){
   df_P <- fread(paste0(setting, "PRS_results_rsquared_threshold", p_current, "pheno_P.txt"))
   df_MG <- fread(paste0(setting, "PRS_results_rsquared_threshold", p_current, "pheno_MG.txt"))
   df_MG_sum <- fread(paste0(setting, "PRS_results_rsquared_threshold", p_current, "pheno_MG_sumPRS.txt"))
+  df_sum_sum <- fread(paste0(setting, "PRS_results_rsquared_threshold", p_current, "pheno_SUM_sumPRS.txt"))
   colnames(df_MG) <- "PRS_MaxH"
-  colnames(df_MG_sum) <- "PRS_SUM"
-  df_merged <- data.frame(df_P, df_MG, df_MG_sum)
-  long_df <- stack(df_merged[, 1:(num_pheno+2)])
+  colnames(df_MG_sum) <- "PRS_SUM_MaxH"
+  colnames(df_sum_sum) <- "PRS_SUM_SUM"
+  df_merged <- data.frame(df_P, df_MG, df_MG_sum, df_sum_sum)
+  long_df <- stack(df_merged[, 1:(num_pheno+3)])
   colnames(long_df) <- c("Rsquared", "Trait")
   long_df$Heritability <- rep(heritabilities, each = nrow(df_merged))
   
@@ -29,7 +31,7 @@ for (p_current in p_thresholds){
     labs(x = "Heritability of Trait", y = expression(paste(R^2))) + 
     theme_bw() +
     theme_classic(base_size = 16)
-  ggsave(filename = paste0(setting, "threshold_" ,p_current , "_PRS_withSUM.png"), width = 6, height = 6)
+  ggsave(filename = paste0(setting, "threshold_" ,p_current , "_PRS_with_sumPRS_MaxH_and_SumTraits.png"), width = 6, height = 6)
 }
 
 
@@ -37,23 +39,27 @@ for (p_current in p_thresholds){
 df_P <- fread(paste0(setting, "PRS_results_rsquared_threshold", '0.000005', "pheno_P.txt"))
 df_MG <- fread(paste0(setting, "PRS_results_rsquared_threshold", '0.000005', "pheno_MG.txt"))
 df_MG_sum <- fread(paste0(setting, "PRS_results_rsquared_threshold", '0.000005', "pheno_MG_sumPRS.txt"))
+df_sum_sum <- fread(paste0(setting, "PRS_results_rsquared_threshold", '0.000005', "pheno_SUM_sumPRS.txt"))
 colnames(df_MG) <- "PRS_MaxH"
 colnames(df_MG_sum) <- "PRS_SUM"
-df_best <- data.frame(df_P, df_MG, df_MG_sum)
+colnames(df_sum_sum) <- "PRS_SUM_SUM"
+df_best <- data.frame(df_P, df_MG, df_MG_sum, df_sum_sum)
 for (p_current in p_thresholds[2:length(p_thresholds)]){
   df_P <- fread(paste0(setting, "PRS_results_rsquared_threshold", p_current, "pheno_P.txt"))
   df_MG <- fread(paste0(setting, "PRS_results_rsquared_threshold", p_current, "pheno_MG.txt"))
   df_MG_sum <- fread(paste0(setting, "PRS_results_rsquared_threshold", p_current, "pheno_MG_sumPRS.txt"))
+  df_sum_sum <- fread(paste0(setting, "PRS_results_rsquared_threshold", p_current, "pheno_SUM_sumPRS.txt"))
   colnames(df_MG) <- "PRS_MaxH"
   colnames(df_MG_sum) <- "PRS_SUM"
-  df_merged <- data.frame(df_P, df_MG, df_MG_sum)
+  colnames(df_sum_sum) <- "PRS_SUM_SUM"
+  df_merged <- data.frame(df_P, df_MG, df_MG_sum, df_sum_sum)
   for (i in 1:ncol(df_merged)){
     if (mean(df_merged[,i], na.rm = T) > mean(df_best[,i], na.rm = T)){
       df_best[,i] <- df_merged[,i]
     }
   }
 }
-long_df <- stack(df_best[, 1:(num_pheno+2)])
+long_df <- stack(df_best[, 1:(num_pheno+3)])
 colnames(long_df) <- c("Rsquared", "Trait")
 long_df$Heritability <- rep(heritabilities, each = nrow(df_best))
 
@@ -62,4 +68,4 @@ ggplot(long_df, aes(x = Heritability, y = Rsquared)) +
   labs(x = "Heritability of Trait", y = expression(paste(R^2))) + 
   theme_bw() +
   theme_classic(base_size = 16)
-ggsave(filename = paste0(setting, "threshold_best_PRS_withSUM.png"), width = 6, height = 6)
+ggsave(filename = paste0(setting, "threshold_best_PRS_with_sumPRS_MaxH_and_SumTraits.png"), width = 6, height = 6)
